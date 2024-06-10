a46c3a9817b30d3e005bb3b7c6f7681b303e8c87
PiotrDrechsler <pdrech@gmail.com>
2024-06-10 19:22:30 +0200

    create/component/LanguageSelector
    ~create logic to select lang
    ~apply styles and import them
    ~add state
    ~add handler to upgrade state
    ~show selectet option in <p>
    ~add "use client"; to make client component

--- nextjs/src/app/components/LanguageSelector/LanguageSelector.tsx
+++ nextjs/src/app/components/LanguageSelector/LanguageSelector.tsx
@@ -1,7 +1,20 @@
+"use client";
+import { useState } from "react";
+
+import styles from "./LanguageSelector.module.css";
+
 export function LanguageSelector() {
+  const [selectedOption, setSelectedOption] = useState();
   return (
     <>
-      <h2>LanguageSelector</h2>
+      <p>Wybrana opcja: {selectedOption}</p>
+      <select
+        className={styles.languageSelector}
+        onChange={(e) => setSelectedOption(e.target.value)}
+      >
+        <option value="PL">Polski</option>
+        <option value="EN">Angielski</option>
+      </select>
     </>
   );
 }
