14a6e4c065ba0470581a2b8f6f24f91a7d71b93c
PiotrDrechsler <pdrech@gmail.com>
2024-06-10 14:50:55 +0200

    import/page.tsx
    import Header and Footer components to page.tsx

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+
 export default function Home() {
   return (
     <main>
+      <Header />
       <h1>Hello next!</h1>
+      <Footer />
     </main>
   );
 }
