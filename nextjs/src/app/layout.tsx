51e99743740e901af9d19d1bc0319109a36ca80f
PiotrDrechsler <pdrech@gmail.com>
2024-06-11 16:15:10 +0200

    create/font
    import roboto and add to body

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
+
+const roboto = Roboto({
+  style: ["normal"],
+  subsets: ["latin"],
+  weight: "300",
+});
 
 export const metadata: Metadata = {
   title: "Hello Next!",
@@ -11,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
-      <body>
+      <body className={roboto.className}>
         <Header />
         {children}
         <Footer />
